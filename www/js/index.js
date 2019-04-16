c64b736c264b3b7a7815ef86fd7552fc0da27d9a
zackhunter006 <48428693+zackhunter006@users.noreply.github.com>
2019-04-16 17:00:46 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=fN1VmJAeplnK5FSQqJ9qgbpjwl4DJncNGHGwh1vO";
         
         $("#loadImage").click(function(){
             $.ajax({
